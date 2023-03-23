9015ad5785180fbcc0aaf397ae6bb6186929297d
Anvil <villatteantoine@gmail.com>
2023-03-23 14:21:29 -0400

    changed for a gradio host + embed

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
-from flask import Flask, render_template, request
-import torch
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from flask import Flask, render_template
 from OpenSSL import SSL
 from flask_talisman import Talisman
 
 app = Flask('Detecteur-ia')
-barthez_tokenizer = AutoTokenizer.from_pretrained("moussaKam/barthez")
-model = AutoModelForSequenceClassification.from_pretrained("Anvil-ML/detecteur-ia")
 
 def interpret_pred_with_sensibility(pred):
     low_bond = -6.748472
@@ -30,22 +26,6 @@ def interpret_pred_with_sensibility(pred):
 @app.route('/')
 def show_predict_isai_form():
     return render_template('predictorform.html')
-
-
-@app.route('/results', methods=['POST'])
-def results():
-
-    form = request.form
-    if request.method == 'POST':
-
-        text_sentence = request.form['text_sentence']
-        input_ids = torch.tensor(
-            [barthez_tokenizer.encode(text_sentence, truncation=True, padding=True, add_special_tokens=True)]
-        )
-        predict = model.forward(input_ids)[0]
-        result = interpret_pred_with_sensibility(predict)
-
-        return render_template('resultsform.html', text_sentence=text_sentence, result=result)
 
 Talisman(app, content_security_policy=None)
 
